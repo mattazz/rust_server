5e882772150c828099c16db9e40992b8106641aa
Matthew Azada <matthewazada101@gmail.com>
2023-11-23 10:27:37 -0500

    added server struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut server = Server::new("127.0.0.1:8080");
+    server.run();
+}
+
+struct Server{
+    addr: String,
+}
+impl Server{
+    fn new(addr: &str) -> Self{
+        Self {
+            addr: addr.to_string()
+        }
+    }
+
+    fn run(&mut self){
+
+    }
 }
