649bc9a0957c384723fd5d62c3f1f9f66188190d
Matthew Azada <matthewazada101@gmail.com>
2023-12-04 13:48:57 -0500

    added trait implementation on requests

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,8 @@
+use crate::http::request;
+use crate::http::Request;
 use ::std::io::Read;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::net::TcpListener;
 pub struct Server {
     addr: String,
@@ -21,7 +25,12 @@ impl Server {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                // Will create a byte slice from the entire array
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
                         }
                         Err(e) => {
                             println!("Failed to read from connection: {}", e);
